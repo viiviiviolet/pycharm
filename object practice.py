698e1ff3886486164c448db1d685a416f3d1c797
Ben Meyer <sparebdmemail@gmail.com>
2021-07-29 12:13:51 -0500

    initial commit

---
+++ object practice.py
@@ -0,0 +1,3 @@
+class Vehicle:
+
+# i dont really have many project ideas
---
+++ tk practice.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+window = tk.Tk()
+entry = tk.Entry(fg="yellow", bg="blue", width=50)
+entry.pack()
+window.mainloop()
